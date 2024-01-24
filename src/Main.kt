8a6c33f0576f8c87adb8b21ce5c5d8b6eaf8b7b6
kynguyen93 <ngocky15793@gmail.com>
2024-01-24 16:00:52 +0700

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    // 1: khai báo biến
+    // từ khóa dùng khai báo biến: var, val
+
+    // var: sử dụng cho biến có thể thay đổi giá trị (mutable)
+    var name: String = "Nguyễn Ngọc Kỳ"
+    name = "Nguyễn Ngọc Kỳ đang học Android"
+
+    // val: sử dụng cho biến không gán lại được giá trị (immutable)
+    val phone: String = "0963159551"
+
+    // phương thức in
+    println(name)
+
+}
